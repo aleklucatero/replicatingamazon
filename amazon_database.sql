a33b6f615e7a2cd4634bc15bbfae1c5872985cb1
andrewasaleh <salehandrew110@gmail.com>
2024-04-28 22:07:10 -0700

    Cart page added

--- amazon_database.sql
+++ amazon_database.sql
@@ -1,92 +1,75 @@
--- CREATE DATABASE IF NOT EXISTS amazon_database;
+CREATE DATABASE IF NOT EXISTS amazon_database;
 
--- USE amazon_database;
+USE amazon_database;
 
--- CREATE TABLE users
--- CREATE TABLE IF NOT EXISTS users (
---     user_id INT AUTO_INCREMENT PRIMARY KEY,
---     username VARCHAR(50) NOT NULL,
---     email VARCHAR(100) NOT NULL UNIQUE,
---     password VARCHAR(100) NOT NULL,
---     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
--- CREATE TABLE address (
---     address_id INT AUTO_INCREMENT PRIMARY KEY,
---     street VARCHAR(255),
---     city VARCHAR(255),
---     state VARCHAR(255),
---     zip VARCHAR(10)
--- );
+CREATE TABLE IF NOT EXISTS address (
+    address_id INT AUTO_INCREMENT PRIMARY KEY,
+    street VARCHAR(255),
+    city VARCHAR(255),
+    state VARCHAR(255),
+    zip VARCHAR(10)
+);
 
--- CREATE TABLE products
--- CREATE TABLE IF NOT EXISTS products (
---     product_id INT AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(255) NOT NULL,
---     description TEXT,
---     price DECIMAL(10, 2) NOT NULL,
---     stock_quantity INT NOT NULL
--- );
+CREATE TABLE IF NOT EXISTS products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL
+);
 
--- -- CREATE TABLE orders
--- CREATE TABLE IF NOT EXISTS orders (
---     order_id INT AUTO_INCREMENT PRIMARY KEY,
---     user_id INT NOT NULL,
---     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     total_amount DECIMAL(10, 2) NOT NULL,
---     status ENUM('pending', 'shipped', 'delivered') DEFAULT 'pending',
---     FOREIGN KEY (user_id) REFERENCES users(user_id)
--- );
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    status ENUM('pending', 'shipped', 'delivered') DEFAULT 'pending',
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
--- CREATE TABLE order_details
--- CREATE TABLE IF NOT EXISTS order_details (
---     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
---     order_id INT NOT NULL,
---     product_id INT NOT NULL,
---     quantity INT NOT NULL,
---     price DECIMAL(10, 2) NOT NULL,
---     FOREIGN KEY (order_id) REFERENCES orders(order_id),
---     FOREIGN KEY (product_id) REFERENCES products(product_id)
--- );
+CREATE TABLE IF NOT EXISTS order_details (
+    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
 
--- CREATE TABLE categories
--- CREATE TABLE IF NOT EXISTS categories (
---     category_id INT AUTO_INCREMENT PRIMARY KEY,
---     name VARCHAR(50) NOT NULL
--- );
+CREATE TABLE IF NOT EXISTS categories (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
 
--- CREATE TABLE product_categories
--- CREATE TABLE IF NOT EXISTS product_categories (
---     product_id INT NOT NULL,
---     category_id INT NOT NULL,
---     PRIMARY KEY (product_id, category_id),
---     FOREIGN KEY (product_id) REFERENCES products(product_id),
---     FOREIGN KEY (category_id) REFERENCES categories(category_id)
--- );
+CREATE TABLE IF NOT EXISTS product_categories (
+    product_id INT NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (product_id, category_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id),
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
 
+DELIMITER //
 
+CREATE PROCEDURE InsertEmployee (
+    IN p_employee_id INT,
+    IN p_employee_name VARCHAR(255),
+    IN p_salary DECIMAL(10, 2)
+)
+BEGIN
+    INSERT INTO employees (employee_id, employee_name, salary)
+    VALUES (p_employee_id, p_employee_name, p_salary);
 
--- Logic on PROCEDURES
--- CREATE OR REPLACE PROCEDURE InsertEmployee (
---     p_employee_id IN NUMBER,
---     p_employee_name IN VARCHAR2,
---     p_salsary IN NUMBER
--- ) AS
--- BEGIN
---     INSERT INTO employees (emplyee_id, employee_name, salary)
---     VALUE (p_employee_id, p_employee_name, p_salsary);
+    SELECT 'Employee inserted successfully.' AS Message;
+END//
 
---     COMMIT;
---     DBMS_OUTPUT.PUT.LINE('Employee inserted successfully.');
--- EXCEPTION
---     WHEN OTHERS THEN
---     ROLLBACK;
---     DBMS_OUTPUT.PUT.LINE('Error inserting employee: ' || SQLERRM);
-
--- END;
--- /
-
--- BEGIN
---     InsertEmployee(105, 'Alek Lucatero', 100000);
--- END;
--- /
+DELIMITER ;
