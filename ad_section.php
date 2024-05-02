22b308f3551fea9d697d407e563bb2309d721748
andrewasaleh <salehandrew110@gmail.com>
2024-05-02 12:54:54 -0700

    ad_section ties to generated product_details page

--- ad_section.php
+++ ad_section.php
@@ -19,64 +19,63 @@
         </div>
 
         <div class="product-comp">
-                    <div class="box box-c">
-                        <h3>Big sound, quality audio</h3>
-                        <div>
-                            <img src="images/microphone.jpg" alt="">
-                        </div>
-                        <a href="mic_ad.php">Shop microphones and more</a>
-                    </div>
-                    <div class="box box-c">
-                        <h3>The latest in tech</h3>
-                        <div>
-                            <img src="images/latest_tech.jpg" alt="">
-                        </div>
-                        <a href="#">Shop new arrivals</a>
-                    </div>
-                    <div class="box box-c">
-                        <h3>A whole new way to work</h3>
-                        <div>
-                            <img src="images/monitor.jpg" alt="">
-                        </div>
-                        <a href="#">Shop personal computers</a>
-                    </div>
-                    <div class="box box-c">
-                        <h3>Play without limits</h3>
-                        <div>
-                            <img src="images/videogames.jpg" alt="">
-                        </div>
-                        <a href="#">Shop gaming deals</a>
-                    </div>
-                    
-                </div>
-
-        <div class="product-comp">
-            <div class="box box-c">
-                <h3>Relieve pain, regain strength</h3>
-                <div>
-                    <img src="images/fitness-equipment.jpg" alt="">
-                </div>
-                <a href="#">Shop pro stimulators now</a>
-            </div>
-            <div class="box box-c">
-                <h3>Hooray for birthdays!</h3>
-                <div>
-                    <img src="images/gift-cards.jpg" alt="">
-                </div>
-                <a href="#">Shop gift cards</a>
-            </div>
-            <div class="box box-c">
-                <h3>Fresh, clean water on tap</h3>
-                <div>
-                    <img src="images/sink-filters.jpg" alt="">
-                </div>
-                <a href="#">Shop under sink filters</a>
-            </div>
-            <div class="box box-c">
-                <h3>Restore your engine</h3>
-                <div>
-                    <img src="images/motor-oils.jpg" alt="">
-                </div>
-                <a href="#">Shop motor oils</a>
-            </div>
+    <div class="box box-c">
+        <h3>Big sound, quality audio</h3>
+        <div>
+            <img src="images/microphone.jpg" alt="">
         </div>
+        <a href="product_detail.php?id=1">Shop microphones and more</a>
+    </div>
+    <div class="box box-c">
+        <h3>The latest in tech</h3>
+        <div>
+            <img src="images/latest_tech.jpg" alt="">
+        </div>
+        <a href="product_detail.php?id=2">Shop new arrivals</a>
+    </div>
+    <div class="box box-c">
+        <h3>A whole new way to work</h3>
+        <div>
+            <img src="images/monitor.jpg" alt="">
+        </div>
+        <a href="product_detail.php?id=3">Shop personal computers</a>
+    </div>
+    <div class="box box-c">
+        <h3>Play without limits</h3>
+        <div>
+            <img src="images/videogames.jpg" alt="">
+        </div>
+        <a href="product_detail.php?id=4">Shop gaming deals</a>
+    </div>
+</div>
+
+<div class="product-comp">
+    <div class="box box-c">
+        <h3>Relieve pain, regain strength</h3>
+        <div>
+            <img src="images/fitness-equipment.jpg" alt="">
+        </div>
+        <a href="product_detail.php?id=5">Shop pro stimulators now</a>
+    </div>
+    <div class="box box-c">
+        <h3>Hooray for birthdays!</h3>
+        <div>
+            <img src="images/gift-cards.jpg" alt="">
+        </div>
+        <a href="product_detail.php?id=6">Shop gift cards</a>
+    </div>
+    <div class="box box-c">
+        <h3>Fresh, clean water on tap</h3>
+        <div>
+            <img src="images/sink-filters.jpg" alt="">
+        </div>
+        <a href="product_detail.php?id=7">Shop under sink filters</a>
+    </div>
+    <div class="box box-c">
+        <h3>Restore your engine</h3>
+        <div>
+            <img src="images/motor-oils.jpg" alt="">
+        </div>
+        <a href="product_detail.php?id=8">Shop motor oils</a>
+    </div>
+</div>
